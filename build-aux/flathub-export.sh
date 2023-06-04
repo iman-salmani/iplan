6aac54143f2b54b8b9a899649e59f79065745dda
iman <contact.imansalmani@gmail.com>
2023-06-04 10:45:09 +0330

    Fix cargo config

--- build-aux/flathub-export.sh
+++ build-aux/flathub-export.sh
@@ -8,6 +8,6 @@ mkdir -p $DIST
 rsync -a --exclude-from='../.gitignore' --exclude='ir.imansalmani.IPlan.Devel.json' --exclude='ir.imansalmani.IPlan.json' --exclude='build-aux' ../ $DIST
 cp ir.imansalmani.IPlan.json $DIST/
 mkdir $DIST/.cargo
-cargo vendor > $DIST/.cargo/config.toml
+cargo vendor "$DIST"/vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > $DIST/.cargo/config.toml
 tar -zcf "$FLATHUB"/iplan.tar.gz $DIST
 sha256sum "$FLATHUB"/iplan.tar.gz > "$FLATHUB"/checksum
