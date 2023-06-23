0609ad339e03f131be8dae8ee9a0ee050970cef9
Iman Salmani <contact.imansalmani@gmail.com>
2023-06-23 18:33:13 +0330

    Drag scroll over CalendarPage

--- src/views/calendar/calendar_page.rs
+++ src/views/calendar/calendar_page.rs
@@ -1,7 +1,9 @@
 use adw::prelude::*;
 use gtk::glib::Properties;
-use gtk::{glib, subclass::prelude::*};
-use std::cell::RefCell;
+use gtk::{gdk, glib, subclass::prelude::*};
+use std::cell::{Cell, RefCell};
+use std::thread;
+use std::time::Duration;
 
 use crate::views::{calendar::TasksList, task::TaskRow};
 
@@ -14,6 +16,10 @@ mod imp {
     pub struct CalendarPage {
         #[property(get, set)]
         pub datetime: RefCell<glib::DateTime>,
+        #[property(get, set)]
+        pub scroll: Cell<i8>,
+        #[template_child]
+        pub scrolled_window: TemplateChild<gtk::ScrolledWindow>,
         #[template_child]
         pub tasks_lists: TemplateChild<gtk::Box>,
     }
@@ -36,12 +42,20 @@ mod imp {
         fn new() -> Self {
             Self {
                 datetime: RefCell::new(glib::DateTime::now_local().unwrap()),
+                scroll: Cell::new(0),
+                scrolled_window: TemplateChild::default(),
                 tasks_lists: TemplateChild::default(),
             }
         }
     }
 
     impl ObjectImpl for CalendarPage {
+        fn constructed(&self) {
+            self.parent_constructed();
+            let obj = self.obj();
+            obj.add_controllers();
+        }
+
         fn properties() -> &'static [glib::ParamSpec] {
             Self::derived_properties()
         }
@@ -107,5 +121,58 @@ impl CalendarPage {
         }
         obj.set_datetime(datetime);
         obj
+    }
+
+    fn add_controllers(&self) {
+        let controller = gtk::DropTarget::new(TaskRow::static_type(), gdk::DragAction::MOVE);
+        controller.set_preload(true);
+        controller.connect_motion(|controller, _, y| {
+            let obj = controller.widget().downcast::<Self>().unwrap();
+            let height = obj.height();
+            if height - (y as i32) < 50 {
+                if obj.scroll() != 1 {
+                    obj.set_scroll(1);
+                    obj.start_scroll();
+                }
+            } else if y < 50.0 {
+                if obj.scroll() != -1 {
+                    obj.set_scroll(-1);
+                    obj.start_scroll();
+                }
+            } else {
+                obj.set_scroll(0)
+            }
+            gdk::DragAction::empty()
+        });
+        controller.connect_leave(|controller| {
+            let obj = controller.widget().downcast::<Self>().unwrap();
+            obj.set_scroll(0);
+        });
+        self.add_controller(controller);
+    }
+
+    fn start_scroll(&self) {
+        let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+        thread::spawn(move || loop {
+            if tx.send(()).is_err() {
+                break;
+            }
+            thread::sleep(Duration::from_secs_f32(0.1));
+        });
+        rx.attach(
+            None,
+            glib::clone!(@weak self as obj => @default-return glib::Continue(false), move |_| {
+                let scroll = obj.scroll();
+                if scroll == 0 {
+                    glib::Continue(false)
+                } else if scroll.is_positive() {
+                    obj.imp().scrolled_window.emit_scroll_child(gtk::ScrollType::StepDown, false);
+                    glib::Continue(true)
+                } else {
+                    obj.imp().scrolled_window.emit_scroll_child(gtk::ScrollType::StepUp, false);
+                    glib::Continue(true)
+                }
+            }),
+        );
     }
 }
